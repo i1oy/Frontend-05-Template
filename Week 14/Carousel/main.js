29555f69cd11b502fbfe06e7e63f3d1d7898c7dd
i1oy <luyuhang1991@gmail.com>
2021-01-18 00:54:50 +0800

    Week 14: Basic Use of JSX

--- Week 14/Carousel/main.js
+++ Week 14/Carousel/main.js
@@ -1,5 +1,74 @@
-for (let i of [1, 2, 3]) {
-    console.log(i);
+function createElement(type, attributes, ...children) {
+    let element = null;
+    if (typeof type === "string") {
+        element = new ElementWrapper(type);
+    } else {
+        element = new type();
+    }
+    for (let name in attributes) {
+        element.setAttribute(name, attributes[name]);
+    }
+    for (let child of children) {
+        if (typeof child === "string") {
+            child = new TextWrapper(child);
+            // debugger;
+        }
+        element.appendChild(child);
+    }
+    return element;
 }
 
-let element = <div />;
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content);
+    }
+    // setAttribute(name, attr) {
+    //     this.root.setAttribute(name, attr);
+    // }
+    appendChild(child) {
+        // this.root.appendChild(child);
+        child.mountTo(this.root);
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+class ElementWrapper {
+    constructor(type) {
+        this.root = document.createElement(type);
+    }
+    setAttribute(name, attr) {
+        this.root.setAttribute(name, attr);
+    }
+    appendChild(child) {
+        child.mountTo(this.root);
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+class Carousel {
+    constructor() {
+        this.root = document.createElement("div");
+    }
+    setAttribute(name, attr) {
+        this.root.setAttribute(name, attr);
+    }
+    appendChild(child) {
+        // this.root.appendChild(child);
+        child.mountTo(this.root);
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+let element = (
+    <div id='a'>
+        <span>Hello, World.</span>
+    </div>
+);
+
+element.mountTo(document.body);
